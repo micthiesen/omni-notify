9e52814ab3f8029a414fe6304e04fac322e80085
Michael Thiesen <michael@understorytech.com>
2024-10-26 15:51:18 -0700

    Removed dotenv dependency to streamline configuration handling

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,13 +1,8 @@
 import { baseConfigSchema } from "@micthiesen/mitools";
-import dotenv from "dotenv";
 import { z } from "zod";
 
-// Load environment variables from .env file if it exists
-dotenv.config();
-
-// Define a Zod schema for the environment variables
 const stringBoolean = (value: string): boolean => value.toLowerCase() === "true";
-const envSchema = baseConfigSchema.extend({
+const configSchema = baseConfigSchema.extend({
 	YT_CHANNEL_NAMES: z
 		.string()
 		.optional()
@@ -15,15 +10,17 @@ const envSchema = baseConfigSchema.extend({
 	OFFLINE_NOTIFICATIONS: z.string().optional().default("true").transform(stringBoolean),
 });
 
-export type Config = z.infer<typeof envSchema>;
+export type Config = z.infer<typeof configSchema>;
 
-const config = envSchema.parse(process.env);
+const config = configSchema.parse(process.env);
 console.log(
 	"Config:",
 	Object.fromEntries(
 		Object.entries(config).map(([key, value]) => [
 			key,
-			key.includes("TOKEN") || key.includes("KEY") ? "***" : value,
+			key.includes("TOKEN") || key.includes("KEY") || key.includes("USER")
+				? "***"
+				: value,
 		]),
 	),
 );
