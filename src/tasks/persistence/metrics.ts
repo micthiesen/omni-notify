ad6d6573f910534eda71a3bbb549ece9cb544bba
Michael Thiesen <michael@understorytech.com>
2024-10-27 16:12:54 -0700

    Updated @micthiesen/mitools to improve data handling functionality

--- src/tasks/persistence/metrics.ts
+++ src/tasks/persistence/metrics.ts
@@ -1,32 +1,30 @@
-import { getDoc, upsertDoc } from "@micthiesen/mitools/docstore";
+import { Entity } from "@micthiesen/mitools/entities";
 import { Logger } from "@micthiesen/mitools/logging";
 
 export type ChannelMetrics = {
 	username: string;
 	maxViewerCount: number;
 };
+export const ChannelMetricsEntity = new Entity<ChannelMetrics, ["username"]>(
+	"channel-metrics",
+	["username"],
+);
 
 const logger = new Logger("Persistence.Metrics");
 
 export function getChannelMetrics(username: string): ChannelMetrics {
-	const pk = metricsPk(username);
-	const metrics = getDoc<ChannelMetrics>(pk);
+	const metrics = ChannelMetricsEntity.get({ username });
 
 	if (metrics) {
-		logger.debug(`Found metrics for ${pk} in DB`, metrics);
+		logger.debug(`Found metrics for ${username} in DB`, metrics);
 		return metrics;
 	}
 
-	logger.debug(`No metrics found in DB for ${pk}; returning default`);
+	logger.debug(`No metrics found in DB for ${username}; returning default`);
 	return { username, maxViewerCount: 0 };
 }
 
 export function upsertChannelMetrics(metrics: ChannelMetrics): void {
-	const pk = metricsPk(metrics.username);
-	upsertDoc<ChannelMetrics>(pk, metrics);
-	logger.debug(`Upserted metrics for ${pk} in DB`, metrics);
-}
-
-function metricsPk(username: string) {
-	return `$channel-metrics#${username}`;
+	ChannelMetricsEntity.upsert(metrics);
+	logger.debug(`Upserted metrics for ${metrics.username} in DB`, metrics);
 }
--- src/tasks/persistence/status.ts
+++ src/tasks/persistence/status.ts
@@ -1,4 +1,4 @@
-import { getDoc, upsertDoc } from "@micthiesen/mitools/docstore";
+import { Entity } from "@micthiesen/mitools/entities";
 import { Logger } from "@micthiesen/mitools/logging";
 import type { Platform } from "../../platforms/index.js";
 
@@ -29,27 +29,26 @@ export type ChannelStatusOffline =
 	  };
 export type ChannelStatus = ChannelStatusLive | ChannelStatusOffline;
 
+export const ChannelStatusEntity = new Entity<ChannelStatus, ["username"]>(
+	"channel-metrics",
+	["username"],
+);
+
 const logger = new Logger("Persistence.Status");
 
 export function getChannelStatus(username: string, platform: Platform): ChannelStatus {
-	const pk = statusPk(username);
-	const status = getDoc<ChannelStatus>(pk);
+	const status = ChannelStatusEntity.get({ username });
 
 	if (status) {
-		logger.debug(`Found status for ${pk} in DB`, status);
+		logger.debug(`Found status for ${username} in DB`, status);
 		return status;
 	}
 
-	logger.debug(`No status found in DB for ${pk}; returning default`);
+	logger.debug(`No status found in DB for ${username}; returning default`);
 	return { username, platform, isLive: false };
 }
 
 export function upsertChannelStatus(status: ChannelStatus): void {
-	const pk = statusPk(status.username);
-	upsertDoc<ChannelStatus>(pk, status);
-	logger.debug(`Upserted status for ${pk} in DB`, status);
-}
-
-function statusPk(username: string) {
-	return `$channel-status#${username}`;
+	ChannelStatusEntity.upsert(status);
+	logger.debug(`Upserted status for ${status.username} in DB`, status);
 }
