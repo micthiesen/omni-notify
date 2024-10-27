e050e99d223749dc0131cac23ca053a4a65197aa
Michael Thiesen <michael@understorytech.com>
2024-10-27 16:18:12 -0700

    Updated @micthiesen/mitools to version 1.2.1 and refactored code

--- src/tasks/persistence/metrics.ts
+++ src/tasks/persistence/metrics.ts
@@ -1,5 +1,4 @@
 import { Entity } from "@micthiesen/mitools/entities";
-import { Logger } from "@micthiesen/mitools/logging";
 
 export type ChannelMetrics = {
 	username: string;
@@ -10,21 +9,11 @@ export const ChannelMetricsEntity = new Entity<ChannelMetrics, ["username"]>(
 	["username"],
 );
 
-const logger = new Logger("Persistence.Metrics");
-
 export function getChannelMetrics(username: string): ChannelMetrics {
 	const metrics = ChannelMetricsEntity.get({ username });
-
-	if (metrics) {
-		logger.debug(`Found metrics for ${username} in DB`, metrics);
-		return metrics;
-	}
-
-	logger.debug(`No metrics found in DB for ${username}; returning default`);
-	return { username, maxViewerCount: 0 };
+	return metrics ?? { username, maxViewerCount: 0 };
 }
 
 export function upsertChannelMetrics(metrics: ChannelMetrics): void {
 	ChannelMetricsEntity.upsert(metrics);
-	logger.debug(`Upserted metrics for ${metrics.username} in DB`, metrics);
 }
--- src/tasks/persistence/status.ts
+++ src/tasks/persistence/status.ts
@@ -1,5 +1,4 @@
 import { Entity } from "@micthiesen/mitools/entities";
-import { Logger } from "@micthiesen/mitools/logging";
 import type { Platform } from "../../platforms/index.js";
 
 export type ChannelStatusLive = {
@@ -34,21 +33,11 @@ export const ChannelStatusEntity = new Entity<ChannelStatus, ["username"]>(
 	["username"],
 );
 
-const logger = new Logger("Persistence.Status");
-
 export function getChannelStatus(username: string, platform: Platform): ChannelStatus {
 	const status = ChannelStatusEntity.get({ username });
-
-	if (status) {
-		logger.debug(`Found status for ${username} in DB`, status);
-		return status;
-	}
-
-	logger.debug(`No status found in DB for ${username}; returning default`);
-	return { username, platform, isLive: false };
+	return status ?? { username, platform, isLive: false };
 }
 
 export function upsertChannelStatus(status: ChannelStatus): void {
 	ChannelStatusEntity.upsert(status);
-	logger.debug(`Upserted status for ${status.username} in DB`, status);
 }
